b2f4295637137247dc8ac96339aa6b2510465fc5
Sallar Kaboli <sallar.kaboli@gmail.com>
2016-12-24 14:48:30 +0200

    Add matrix

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log('hello');
+import { LedMatrix, createStore } from 'led-matrix';
+import './style.css';
+
+// Constants
+const LED_WIDTH = 32;
+const LED_HEIGHT = 16;
+
+// Create canvas
+const $wrapper = document.createElement('div');
+const $canvas = document.createElement('canvas');
+
+$wrapper.id = 'wrapper';
+$wrapper.appendChild($canvas);
+document.body.appendChild($wrapper);
+
+// Create LED matrix
+const matrix = new LedMatrix($canvas, {
+  x: LED_WIDTH,
+  y: LED_HEIGHT,
+  pixelWidth: 30,
+  pixelHeight: 30
+});
+const store = createStore(LED_WIDTH, LED_HEIGHT);
+
+// Render
+matrix.setData(store.matrix);
+matrix.render();
--- webpack.config.js
+++ webpack.config.js
@@ -14,9 +14,11 @@ module.exports = {
       {
         test: /\.js/,
         exclude: /node_modules/,
-        use: [
-          'babel-loader'
-        ]
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css/,
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
