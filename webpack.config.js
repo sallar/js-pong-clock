2b3ed6246895f83e56eb6d8c2b8fa42134a90c56
Sallar Kaboli <sallar.kaboli@gmail.com>
2016-12-24 12:42:59 +0200

    Add basic structure

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader'
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin()
+  ],
+
+  devtool: 'cheap-module-source-map'
+};
